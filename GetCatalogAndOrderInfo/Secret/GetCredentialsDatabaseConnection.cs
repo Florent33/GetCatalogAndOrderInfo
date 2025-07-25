2583010dc1edf239d1d899761f101800654b78e5
Florent FERRAND <ferrand.florent@gmail.com>
2025-07-25 17:07:14 +0200

    Delete GetCatalogAndOrderInfo/Secret directory

--- GetCatalogAndOrderInfo/Secret/GetCredentialsDatabaseConnection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GetCatalogAndOrderInfo.Secret
-{
-    public static class GetCredentialsDatabaseConnection
-    {
-        public static readonly string connectionString = "Data Source=LDC-DATAWARE01.cdbdx.biz\\DWH;Failover Partner=LDC-DATAWARE01.cdbdx.biz\\DWH;Initial Catalog=CDISCOUNT_EXTRACTIONS;User ID=AppExportCatalog;Password=Pq7Qjue7!!;enlist=false;App=EntityFramework;TrustServerCertificate=true";
-    }
-}
